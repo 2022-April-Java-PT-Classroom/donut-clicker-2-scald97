0a98d897a8ef28e77f19a96d74fc47a2e2998dd8
Serena Caldarella <96984066+scald97@users.noreply.github.com>
2022-07-12 18:46:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+import DonutMaker from "./DonutMaker";
+import index from "./index.html";
+renderpage();
+
+function renderpage() {}
+
+// const container = (document.querySelector(".container").innerText =
+//   "mmmmmmmmm doughnuts.... ahhhhhhh!");
+
+const createDonut = new DonutMaker(0, 0, 0);
+const addDonut = document.querySelector("#clicks"); // Display
+const donutCount = document.querySelector("button");
+
+updateDisplay();
+
+
+//buttons
+
+donutCount.addEventListener("click", () => {
+  createDonut.click();
+  updateDisplay();
+});
+
+// function buyAutoClicker() {
+//   document.getElementById("auto-clicker").innerHTML = click;
+// }
+
+// function buyDonutMultiplier() {
+//   document.getElementById("multiplier").innerHTML = click;
+// }
+
+function updateDisplay() {
+  addDonut.innerText = "Donuts:" + createDonut.numDonuts;
+}
